9c705e421b8d1e868c1bf8d9315e9a75ace380fe
Aryan Daftari <aryandaftari7@gmail.com>
2024-07-15 14:03:53 +0530

    Added Projects Section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 function App() {
   return (
 <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -16,6 +17,7 @@ function App() {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
       </div>
     </div>
   );
