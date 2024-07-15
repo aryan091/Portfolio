c34b48fab8b14342a5f3044c1153fd0801135b38
Aryan Daftari <aryandaftari7@gmail.com>
2024-07-15 13:41:38 +0530

    Experience Added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 function App() {
   return (
 <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
     </div>
   );
