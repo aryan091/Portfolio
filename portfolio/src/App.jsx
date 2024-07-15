cf3e39f7f56b74657388ac61531c27c2261a3428
Aryan Daftari <aryandaftari7@gmail.com>
2024-07-15 20:50:21 +0530

    Added CSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 custom-scrollbar  ">
       <div className=" fixed top-0 z-0 h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] custom-scrollbar"></div>
       <div className="relative z-10">
-        <div className=" container mx-auto px-8 custom-scrollbar lg:mx-20">
+        <div className=" container mx-auto px-8 custom-scrollbar lg:mx-8">
           <Navbar />
           <Hero />
           <About />
