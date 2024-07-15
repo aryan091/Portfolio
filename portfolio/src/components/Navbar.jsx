164961d1271e857bdc8116b1d6f6af681d061793
Aryan Daftari <aryandaftari7@gmail.com>
2024-07-15 20:01:52 +0530

    Added More Links

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import {FaLinkedin} from 'react-icons/fa'
 import {FaGithub} from 'react-icons/fa'
-
 import {FaSquareXTwitter} from 'react-icons/fa6'
-
+import { SiLeetcode } from "react-icons/si";
 import {FaInstagram} from 'react-icons/fa'
+import { SiGeeksforgeeks } from "react-icons/si";
 
 const Navbar = () => {
   return (
@@ -18,6 +18,9 @@ const Navbar = () => {
     <FaGithub className='cursor-pointer hover:text-pink-500' onClick={() => window.open('https://github.com/aryan091', '_blank')}/>
     <FaSquareXTwitter className='cursor-pointer hover:text-pink-500' onClick={() => window.open('https://x.com/aryan75529', '_blank')} />
     <FaInstagram className='cursor-pointer hover:text-pink-500' onClick={() => window.open('https://www.instagram.com/_aryan_0007', '_blank')}/>
+    <SiLeetcode className='cursor-pointer hover:text-pink-500' onClick={() => window.open('https://leetcode.com/u/aryandaftari7/', '_blank')}/>
+    <SiGeeksforgeeks className='cursor-pointer hover:text-pink-500' onClick={() => window.open('https://www.geeksforgeeks.org/user/aryandaftari7/', '_blank')}/>
+
 </div>
 </nav>  
 )
